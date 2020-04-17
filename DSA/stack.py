584503254d8186de9631f55baa4d0d10bad2dc88
yassin549 <khoualdiyassin26@gmail.com>
2020-04-17 09:50:00 +0500

    Connected UI components to backend logic for scheduling

---
+++ DSA/stack.py
@@ -0,0 +1,27 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+    def display(self):
+        return self.items
+
+    def insert_at_beginning(self, item):
+        # Insert an element at the beginning of the stack
+        self.items.insert(0, item)
